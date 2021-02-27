9aa124b578f047e3c2a9b1c07a13c7be2075659d
Hanna Putiprawan <hputiprawan2@gmail.com>
2021-02-27 02:30:44 -0800

    [Add] Loading ML

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    let imagePicker = UIImagePickerController()
+    
+    private let imagePicker = UIImagePickerController()
+    private var pickedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +27,40 @@ class ViewController: UIViewController {
         present(imagePicker, animated: true, completion: nil)
     }
     
+    private func detect(flowerImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: MLModel(contentsOf: FlowerClassifier.urlOfModelInThisBundle)) else {
+            fatalError("Load CoreML Model failed!")
+        }
+        
+        // Make a request ask model to classify data
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            self.navigationItem.title = result.first?.identifier ?? "Unidentify!"
+        }
+        
+        // Perform classify image
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+    }
 }
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 //        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
-        let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]
-        
-        imageView.image = userPickedImage as? UIImage
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert image to CIImage")
+            }
+            pickedImage = userPickedImage
+            detect(flowerImage: ciImage)
+        }
         imagePicker.dismiss(animated: true, completion: nil)
     }
 }
